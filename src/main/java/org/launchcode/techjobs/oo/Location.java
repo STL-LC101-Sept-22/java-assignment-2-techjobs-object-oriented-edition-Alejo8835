1284f041eb46b94dc44fb1fbda39c45768d24806
Alejandro Pedreros <pedrerosalejandro_21@hotmail.com>
2023-02-08 20:01:19 -0600

    Added constructors, setters, getters, ToString, equals, and hashCode methods to Location, CoreCompetency, and PostionType classes[D.

--- src/main/java/org/launchcode/techjobs/oo/Location.java
+++ src/main/java/org/launchcode/techjobs/oo/Location.java
@@ -15,12 +15,16 @@ public class Location {
 
     // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
     //  constructor should also call the empty constructor in order to initialize the 'id' field.
-
+    public Location(String Value) {
+        this();
+        this.value = Value;
+    }
 
     // Custom toString, equals, and hashCode methods:
 
     @Override
     public String toString() {
+
         return value;
     }
 
@@ -34,20 +38,24 @@ public class Location {
 
     @Override
     public int hashCode() {
+
         return Objects.hash(getId());
     }
 
     // Getters and Setters:
 
     public int getId() {
+
         return id;
     }
 
     public String getValue() {
+
         return value;
     }
 
     public void setValue(String value) {
+
         this.value = value;
     }
 
