da965c03512b92e9193abbfcd5ab6a2b63804895
Alejandro Pedreros <pedrerosalejandro_21@hotmail.com>
2023-02-11 19:22:01 -0600

    Use Tested thee  empty and full constructor [D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[3~[3~[3~[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C empty and the five field constructors as well as the test equal method

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,7 +1,13 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
+
 
 /**
  * Created by LaunchCode
@@ -9,4 +15,42 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class JobTest {
 
+    Job job1;
+    Job job2;
+    Job job3;
+
+    @Before
+    public void createTestData() {
+        job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        job3 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
+
+    }
+
+    @Test
+    public void testSettingJobId() {
+        String spec = "First constructor sets the job id and verifies that the IDs of the two objects are distinct";
+        job1.getId();
+        job3.getId();
+        assertNotEquals(job1, job3);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        assertTrue(job1 instanceof Job);
+        assertEquals("Product tester", job1.getName());
+        assertTrue(job1.getEmployer() instanceof Employer);
+        assertEquals("ACME", job1.getEmployer().getValue());
+        assertTrue(job1.getLocation() instanceof Location);
+        assertEquals("Desert", job1.getLocation().getValue());
+        assertTrue(job1.getPositionType() instanceof PositionType);
+        assertEquals("Quality control", job1.getPositionType().getValue());
+        assertTrue(job1.getCoreCompetency() instanceof CoreCompetency);
+        assertEquals("Persistence", job1.getCoreCompetency().getValue());
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        assertFalse(job1.equals(job2));
+    }
 }
